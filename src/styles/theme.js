512702b964eca8ea52dcbb1f8f4df15b5588cfb7
Alvaro Montero <alvaro@montero-design.com>
2020-07-02 20:04:17 -0400

    feat(theme): start base styles

--- src/styles/theme.js
+++ src/styles/theme.js
@@ -1,6 +1,29 @@
 const theme = {
+  fontSize: {
+    xs: 10,
+    s: 14,
+    m: 16,
+    l: 20,
+    xl: 32,
+    xxl: 46,
+  },
+  fontWeight: {
+    light: 200,
+    normal: 400,
+    bold: 600,
+    black: 800,
+  },
   palette: {
     dark: '#2b303b',
+    light: '#F1F1F1',
+  },
+  space: {
+    xs: 4,
+    s: 8,
+    m: 16,
+    l: 32,
+    xl: 64,
+    xxl: 128,
   },
 };
 
